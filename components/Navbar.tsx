cd133eda0ca7b48dd8a7e1b9f267734ccbd45886
Ibranista <ibranista88@gmail.com>
2023-01-17 15:12:37 +0100

    add navbar items that show profile and write posts by checking if there's a user.

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  const { user, username } = {};
+  return (
+    <>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link href="/">
+              <button className="btn-logo">FEED</button>
+            </Link>
+          </li>
+
+          {username && (
+            <>
+              <li className="push-left">
+                <Link href="/admin">
+                  <button className="btn-blue">Write Posts</button>
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${username}`}>
+                  <button>Profile</button>
+                  <Image src={user?.photoURL} alt="user profile" />
+                </Link>
+              </li>
+            </>
+          )}
+
+          {!username && (
+            <li>
+              <Link href="/enter">
+                <button className="btn-blue">Log in</button>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
