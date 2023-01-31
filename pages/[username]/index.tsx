fc4991c3b6653a37dc02624b60ac5e1526c5ee60
Ibranista <ibranista88@gmail.com>
2023-01-31 15:55:43 +0100

    add helper method to convert post data to json.

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,10 +1,35 @@
 import UserProfile from "@/components/UserProfile";
+import getUserWithUsername from "@/helpers/getUserWithUsername";
+import { firestore } from "@/lib/firebase";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import React from "react";
 
-export async function getServerSideProps({ query }) {
+export async function getServerSideProps({ query }: any) {
   const { username } = query;
 
+  const userDoc = await getUserWithUsername(username);
 
+  // JSON Sserializable data means that the data does not contain functions or Date objects
+  let user = null;
+  let posts: object = {};
+
+  if (userDoc) {
+    user = userDoc.docs;
+    const postsQuery = query(
+      collection(firestore, "posts"),
+      where("username", "==", "ibranista"),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map  ;
+  }
   return {
     props: { user, posts },
   };
