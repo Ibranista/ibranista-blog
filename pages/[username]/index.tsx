15bf99941380f614eb17646ee31f95f3f755afbd
Ibranista <ibranista88@gmail.com>
2023-02-02 12:20:42 +0100

    create helper methods to fetch username document and convert timestamps to numbers.

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,25 +1,39 @@
 import PostFeed from "@/components/PostFeed";
 import UserProfile from "@/components/UserProfile";
-import getUserWithUsername from "@/helpers/getUserWithUsername";
+import { getUserWithUsername, postToJSON } from "@/helpers/getUserWithUsername";
 import { firestore } from "@/lib/firebase";
 import {
   collection,
   getDocs,
   limit,
   orderBy,
-  query,
+  query as Query,
   where,
 } from "firebase/firestore";
 import React from "react";
-
-export async function getServerSideProps({ query }: any) {
+export async function getServerSideProps({ query }: { query: any }) {
   const { username } = query;
+  const userDoc = await getUserWithUsername(username);
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = Query(
+      collection(firestore, "posts"),
+      where("author.uid", "==", userDoc.id),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  }
+
   return {
     props: { user: "ibrahim", posts: { name: "Ibrahim" } },
   };
 }
 
-function UserProfilePage({ user, query }: { user: string }) {
+function UserProfilePage({ user }: { user: string }) {
   return (
     <>
       <main>
