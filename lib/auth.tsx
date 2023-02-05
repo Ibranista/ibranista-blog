145385f967d7b9c003d31032590e9459576918ef
Ibranista <ibranista88@gmail.com>
2023-02-05 07:08:17 +0100

    use useEffect hook to st username from firestore and unsubscribe listening.

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -1,15 +1,37 @@
 import SignINOut from "@/auth/SignInOut";
-import { auth } from "./firebase";
-import { createContext, SetStateAction, useContext, useState } from "react";
+import { auth, firestore } from "./firebase";
+import {
+  createContext,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 // import components
 import CreateUser from "./CreateUser";
 import SignInWithEmail from "@/auth/SignWithEmail";
+import { doc, onSnapshot } from "firebase/firestore";
 export const UserContext = createContext<any>({ user: null, username: "" });
 export default function Authentication() {
   let [user] = useAuthState(auth);
-  const [username] = useState<SetStateAction<any>>(null);
+  const [username, setUsername] = useState<SetStateAction<any>>(null);
   const { SignInButton, SignOut } = SignINOut();
+
+  useEffect(() => {
+    let unsubscribe;
+
+    if (user) {
+      const ref = doc(firestore, "users", user.uid);
+      unsubscribe = onSnapshot(ref, (doc) => {
+        setUsername(doc.data()?.username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    return unsubscribe;
+  }, [user]);
 
   return {
     SignInButton,
