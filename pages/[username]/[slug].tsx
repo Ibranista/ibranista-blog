3410acaa842ff1ff101a4c1e3fef96dd3c21c342
Ibranista <ibranista88@gmail.com>
2023-02-18 09:50:07 +0100

    return props and revalidate

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,4 +1,24 @@
+import { getUserWithUsername } from "@/helpers/getUserWithUsername";
+import { firestore, PostToJSON } from "@/lib/firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
 import React from "react";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+  // returns array of objects containing user properties
+  let post;
+  let path;
+  if (userDoc) {
+    const postRef = doc(firestore, userDoc.ref.path, "posts", slug);
+    const post = PostToJSON(await getDoc(postRef));
+    const path = postRef.path;
+  }
+  return {
+    props: { post, path },
+    revalidate: 5000, //milliseconds
+  };
+}
 
 function PostPage() {
   return (
