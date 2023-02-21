faeb59b1a0822ce8dc6ec7cc0d31c92356312313
Ibranista <ibranista88@gmail.com>
2023-02-21 11:53:55 +0100

    add metatag reusable component.

---
+++ components/Metatags.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function MetaTags({ title, description, image }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      {/* add some meta tags for sharing content over twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ibranista" />
+      <meta name="twitter:creator" content="@ibranista" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      {/* add some meta properties */}
+      <meta property="og:url" content="https://ibranista.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={image} />
+    </Head>
+  );
+}
