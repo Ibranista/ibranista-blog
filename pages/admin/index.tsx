6e77cd8e535448edd0cd9ef8490432cb0d92d707
Ibranista <ibranista88@gmail.com>
2023-02-24 09:14:26 +0100

    add a postlist component to query posts snapshot on realtime and and render it through postFeed component.

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,15 +1,47 @@
 import AuthCheck from "@/components/AutchCheck";
+import PostFeed from "@/components/PostFeed";
+import { UseAuth } from "@/lib/auth";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { kebabCase, orderBy } from "lodash";
+import { toast } from "react-hot-toast";
 import React from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { auth, firestore } from "@/lib/firebase";
+import { updateCurrentUser } from "firebase/auth";
+import {
+  useCollection,
+  useCollectionData,
+  useDocument,
+} from "react-firebase-hooks/firestore";
 function AdminPostsPage(props) {
   return (
     <>
       <main>
         <AuthCheck>
-          <h1>Hello mate Welcome</h1>
+          {/* <PostList /> */}
+          {/* to show all posts created by user */}
+          {/* <CreateNewPost /> */}
+          {/* contains form to create a new post */}
         </AuthCheck>
       </main>
+      <PostList />
     </>
   );
 }
 
 export default AdminPostsPage;
+
+function PostList() {
+  let postRef = collection(firestore, "users", auth.currentUser.uid, "posts");
+  let q = query(postRef, orderBy("createdAt", "desc"));
+  let [querySnapshot] = useCollection(q);
+  // let posts = querySnapshot?.docs.map((doc) => doc.data());
+  let posts = useCollectionData(q)[0];
+  return (
+    <>
+      <h1>Manage Your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
